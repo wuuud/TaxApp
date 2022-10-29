b0a1d0b140f1faca5e1f77633dd6f39ccae06bb1
“wuuud” <syrdiy@gmail.com>
2022-10-29 18:05:29 +0900

    課題修正完了

--- TaxApp/ViewController.swift
+++ TaxApp/ViewController.swift
@@ -9,19 +9,19 @@ import UIKit
 
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController,  UITableViewDelegate {
     //1.初期設定
-    //    var totalButtonItem: UIBarButtonItem! // 追加ボタン
-    var inputText = ""
-    var beforeTax8: Double = 0.0
-    var beforeRoundTax10: Double = 0.0
     //  オプショナルではないので、初期値を入れてあげる
     var taxArray: [Double] = []
     var userDefaults = UserDefaults.standard
     //2.紐付け
     @IBOutlet weak var priceText: UITextField!
-    //    @IBAction func totalButton(_ sender: UIBarButtonItem) {
-    //    }
+    //editing changedを追加したので、すぐにsegmentを動かさなくても税込価格が表示できる
+    @IBAction func priceText(_ sender: UITextField) {
+        //税率計算と表示
+        calc()
+        print(priceText.text!)
+    }
     @IBOutlet weak var taxPriceLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBAction func addPrice(_ sender: UIButton) {
@@ -46,44 +46,73 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //segmentedhttps://qiita.com/rea_sna/items/7e472af2ce8d03831b55
     @IBOutlet weak var selectSegmentedControl: UISegmentedControl!
     @IBAction func tappedSegmentedControl(_ sender: UISegmentedControl) {
-        if priceText.text != nil && Double(priceText.text!) != nil {
-            switch sender.selectedSegmentIndex {
-                //https://ios-docs.dSev/swiftui-part5/
-            case 0:
-                calc(num: 1.1)
-            case 1:
-                calc(num: 1.08)
-            default:
-                priceText.text = String(0)
-            }
-        }else {
-            priceText.text = String(0)
-            showAlert(message: "数字を入力してください")
-        }
+        calc()
     }
-    
     
     //3−2.viewDid
     override func viewDidLoad() {
         super.viewDidLoad()
+        //税率の切替部分を触らずとも計算できるよう
+        //textField.delegate = self
+        tableView.delegate = self
         tableView.dataSource = self
-        // ナビゲーションバー にボタンを追加
-        //        self.navigationItem.rightBarButtonItem = totalButtonItem
         priceText.placeholder = "数字を入力"
         //まずはUserDefaultsからすでに入力されているpriceをtaxPriceに読み込む
         if userDefaults.object(forKey: "add") != nil{
-        //                                              配列に入れるので[]
+            //                                              配列に入れるので[]
             taxArray = userDefaults.object(forKey: "add") as! [Double]
         }
         print(taxArray)
         tableView.reloadData()
     }
-    
     //テキストフィールドに最初からフォーカスが表示される
     override func viewWillAppear(_ animated: Bool) {
         priceText.becomeFirstResponder()
     }
     
+    //4.関数
+    //4-1.func
+    //テキストを編集するたび(文字が入力されるたび)呼ばれるメソッド。 1文字遅れでの表示なので使いにくい https://naoya-ono.com/swift/uitextfield-delegate/#7_textField 7番目
+    //    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    //        return true
+    //    }
+    //4-2.func
+    //①アラート  決まり文句
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
+    //②計算式
+    func calcunwrapped(num: Double){
+        if let unwrappedPriceText = priceText.text {
+            //https://ios-docs.dSev/swiftui-part5/
+            let price = Double(unwrappedPriceText)
+            let tax = price! * (num)
+            self.taxPriceLabel.text = String(tax)
+        }else {
+            priceText.text = String(0)
+        }
+    }
+    //③セグメントによる場合分け
+    func calc() {
+        if priceText.text != nil && Double(priceText.text!) != nil {
+            switch selectSegmentedControl.selectedSegmentIndex {
+                //https://ios-docs.dSev/swiftui-part5/
+            case 0:
+                calcunwrapped(num: 1.1)
+            case 1:
+                calcunwrapped(num: 1.08)
+            default:
+                break
+            }
+        }
+    }
+}
+
+//5.classの外
+extension ViewController: UITableViewDataSource {
     //3-3.tableview用 https://www.sejuku.net/blog/36626
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -99,31 +128,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = String(taxArray[indexPath.row])
         print(String(taxArray[indexPath.row]))
         return cell
-    }
-//テキストを編集するたび(文字が入力されるたび)呼ばれるメソッド。 https://naoya-ono.com/swift/uitextfield-delegate/#7_textField 7番目
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-      print("変更あり")
-      return true
-    }
-    
-    //4.関数
-    //4-2.func
-    //①アラート  決まり文句
-    func showAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
-        alert.addAction(close)
-        present(alert, animated: true, completion: nil)
-    }
-    //②計算用
-    func calc(num: Double) {
-        if let unwrappedPriceText = priceText.text {
-            //https://ios-docs.dSev/swiftui-part5/
-            let price = Double(unwrappedPriceText)
-            let tax = price! * (num)
-            self.taxPriceLabel.text = String(tax)
-        }else {
-            priceText.text = String(0)
-        }
     }
 }
